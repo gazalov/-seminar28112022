5507d29401951bb8881b8d8045b2f874b8428dea
a.gazalov <gazalov@list.ru>
2022-11-28 21:10:42 +0300

    Task 17

---
+++ Task17/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+int DataEntryXY(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void FindQuadrant(int x, int y)
+{
+    if((x>0) && (y>0)) Console.WriteLine("Это первый квадрант.");
+    if((x<0) && (y>0)) Console.WriteLine("Это второй квадрант.");
+    if((x<0) && (y<0)) Console.WriteLine("Это третий квадрант.");
+    if((x>0) && (y<0)) Console.WriteLine("Это четвертый квадрант.");
+}
+
+int x = DataEntryXY("Введите координаты x = ");
+//Console.WriteLine(x);
+int y = DataEntryXY("Введите координаты y = ");
+//Console.WriteLine(y);
+
+FindQuadrant(x, y);
