29f0f3366434d806a2c6a6fec576606827e85ad7
a.gazalov <gazalov@list.ru>
2022-11-29 15:33:49 +0300

    Task 23 homework

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int DataEntry(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void Calc(int num)
+{
+    int i = 1;
+    while (i <= num)
+    {
+        Console.Write(i * i * i + " ");
+        i++;
+    }
+}
+
+int numberN = DataEntry("Введите N: ");
+Calc(numberN);
